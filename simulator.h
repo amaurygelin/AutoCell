a16d2c3731d48b41874cf3c855407ef640ba8f8d
Wael-Hamdan <39374662+Wael-Hamdan@users.noreply.github.com>
2018-06-05 19:04:21 +0200

    Update simulator.h

--- simulator.h
+++ simulator.h
@@ -5,14 +5,17 @@
 #include "Automaton.h"
 
 class Simulator{
+    static Simulator* uniqueSim;
     const Automaton& automaton;
     const Grid* initial_grid;
     unsigned int max_grids;
     std::vector<Grid*> grids;
     unsigned int cur_rank;
-public:
     Simulator(const Automaton& autom, unsigned int max_g=2);
     Simulator(const Automaton& autom, const Grid& initial_g,unsigned int max_g=2);
+    virtual ~Simulator();
+    void operator=(const Simulator&);
+public:
     void setInitialGrid(const Grid& grid);
     void next();
     void run(unsigned int nbSteps);
@@ -21,10 +24,14 @@ public:
     unsigned int getCurRank() const{return cur_rank;}
     unsigned int getMaxGrids() const{return max_grids;}
     void build(unsigned int c);
-    ~Simulator();
     void displayGrids();
+    static Simulator& getSimulator(const Automaton& autom, const Grid& initial_g, unsigned int max_g);
+    static Simulator& getSimulator(const Automaton& autom, unsigned int max_g);
+    static void freeInstance();
 };
 
 std::ostream & operator<<(std::ostream & f,const Simulator & s);
+
+
 
 #endif // SIMULATOR_H_INCLUDED

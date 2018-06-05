52bf436dc3219ae7e3ff4875938fd9214a154620
Wael-Hamdan <39374662+Wael-Hamdan@users.noreply.github.com>
2018-06-05 19:07:28 +0200

    Update Automaton.h

--- AutoCell/Automaton.h
+++ AutoCell/Automaton.h
@@ -13,6 +13,7 @@ class Automaton {
         Automaton(unsigned int dim, unsigned int nb_s): dimension(dim), nb_states(nb_s) {}
         virtual void transition(const Grid& src, Grid& dest) const = 0;
         virtual std::ostream& display(std::ostream& f = std::cout) const; //is abstract but has a definition
+        unsigned int getDim() const{return dimension;}
 };
 
 std::ostream& operator<<(std::ostream& f, Automaton const& a);

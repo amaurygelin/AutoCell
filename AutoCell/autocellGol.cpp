9bd8c1d1def04738bb9e77b4cd6625a52dbe3bf8
Wael-Hamdan <39374662+Wael-Hamdan@users.noreply.github.com>
2018-06-17 11:24:46 +0200

    Update autocellGol.cpp

--- AutoCell/autocellGol.cpp
+++ AutoCell/autocellGol.cpp
@@ -6,11 +6,17 @@
 #include "symmetricDGridGenerator.h"
 #include "symmetricHGridGenerator.h"
 #include "symmetricVGridGenerator.h"
+#include "saveuicontext.h"
+#include "dbManager.h"
 
-AutoCellGol::AutoCellGol(QWidget *parent) : QWidget(parent) {
+extern DbManager dbTest;
+
+
+AutoCellGol::AutoCellGol(QWidget *parent) : QWidget(parent){
     stepSimulation=false;
     playSimulation=false;
-
+    timer = new QTimer(this);
+    connect(timer, SIGNAL(timeout()), this, SLOT(launchSimulationStep()));
 
     namel = new QLabel("Game of life", this);
 
@@ -42,7 +48,7 @@ AutoCellGol::AutoCellGol(QWidget *parent) : QWidget(parent) {
     nb_step = new QSpinBox(this);
     nb_step->setRange(0, 255);
     nb_step->setValue(0);
-    nb_stepl = new QLabel("Number of steps", this);
+    nb_stepl = new QLabel("Simulator's number of grids", this);
 
     numc = new QVBoxLayout;
     numc->addWidget(namel);
@@ -66,7 +72,24 @@ AutoCellGol::AutoCellGol(QWidget *parent) : QWidget(parent) {
     couche = new QVBoxLayout;
     couche->addLayout(numeroc);
 
+
+
     depart=nullptr;
+
+    saveAutomaton = new QPushButton("Save Automaton", this);
+    connect(saveAutomaton, SIGNAL(clicked()), this, SLOT(saveAutom()));
+    couche->addWidget(saveAutomaton);
+
+
+    loadAutomatonl = new QLabel("Choose an automaton to load (nb neighbours to born, min neigbours, max neigbours)", this);
+    couche->addWidget(loadAutomatonl);
+    loadAutomaton = new QComboBox(this);
+    couche->addWidget(loadAutomaton);
+    connect(loadAutomaton, SIGNAL(activated(int)), this, SLOT(choiceAutomChanged()));
+
+    loadAutomList();
+
+
     grid = new QPushButton("New Grid", this);
     connect(grid, SIGNAL(clicked()), this, SLOT(newGrid()));
     couche->addWidget(grid);
@@ -82,15 +105,29 @@ AutoCellGol::AutoCellGol(QWidget *parent) : QWidget(parent) {
 
 
     simLayout = new QHBoxLayout;
-    simulation = new QPushButton("Play simulation", this);
+    simulation = new QPushButton("Start/Resume continuous simulation", this);
     connect(simulation, SIGNAL(clicked()), this, SLOT(launchSimulation()));
     simLayout->addWidget(simulation);
+
+    pauseButton = new QPushButton("Pause Simulation",this);
+    connect(pauseButton,SIGNAL(clicked()), this, SLOT(pause()));
+    simLayout->addWidget(pauseButton);
+
+    speed = new QSpinBox(this);
+    speed->setRange(0, 50);
+    speed->setValue(0);
+    speedl = new QLabel("Simulation speed (in sec)", this);
+    simLayout->addWidget(speedl);
+    simLayout->addWidget(speed);
+
     simulationStep = new QPushButton("Simulation step by step", this);
     connect(simulationStep, SIGNAL(clicked()), this, SLOT(launchSimulationStep()));
     simLayout->addWidget(simulationStep);
     couche->addLayout(simLayout);
 
     setLayout(couche);
+    loadSettings();
+    connect(QApplication::instance(), SIGNAL(aboutToQuit()), this, SLOT(saveSettings()));
 }
 
 void AutoCellGol::cellActivation(const QModelIndex& index) {
@@ -264,29 +301,14 @@ void AutoCellGol::launchSimulation() {
     GameOfLife gol(nb_n->value(),min_n->value(),max_n->value());
     // on construit l'objet simulateur correspondant
     Simulator* sim = &(Simulator::getSimulator(gol,g1,nb_step->value()));
-
     // on applique les transitions au simulateur en affichant le résultat dans l'interface graphique
-    for(unsigned int step = 0; step < nb_step->value(); ++step) {
-        // on applique la transition
-        sim->next();
-        // on récupère le dernier état
-        const Grid& g = sim->last();
-        // on l'affiche
-        for(unsigned int i = 0; i < dim1->value(); ++i) {
-            for(unsigned int j = 0; j < dim2->value(); ++j) {
-                if(g.getCell(i,j)== 1){
-                     depart->item(i, j)->setText("_");
-                     depart->item(i, j)->setBackgroundColor("black");
-                     depart->item(i, j)->setTextColor("black");
-                 } else {
-                     depart->item(i, j)->setText("");
-                     depart->item(i, j)->setBackgroundColor("white");
-                     depart->item(i, j)->setTextColor("white");
-                 }
-            }
-        }
-    }
+
+
+
+    timer->start(speed->value()*1000);
+
 }
+
 
 void AutoCellGol::launchSimulationStep() {
     stepSimulation=true;
@@ -328,3 +350,123 @@ void AutoCellGol::launchSimulationStep() {
         }
     }
 }
+
+void AutoCellGol::pause(){
+    timer->stop();
+}
+
+void AutoCellGol::saveSettings(){
+    int  nb_n_val = nb_n->value();
+    saveUiContext("nb_n_key_gol",nb_n_val);
+
+    int  min_n_val = min_n->value();
+    saveUiContext("min_n_key_gol",min_n_val);
+
+    int  max_n_val = max_n->value();
+    saveUiContext("max_n_key_gol",max_n_val);
+
+    int  dim1_val = dim1->value();
+    saveUiContext("dim1_key_gol",dim1_val);
+
+    int  dim2_val = dim2->value();
+    saveUiContext("dim2_key_gol",dim2_val);
+
+    int  nb_step_val = nb_step->value();
+    saveUiContext("nb_step_key_gol",nb_step_val);
+
+    int choice_grid_gol_val = choiceGrid->currentIndex();
+    saveUiContext("choice_grid_gol_key",choice_grid_gol_val);
+
+    for(unsigned int i = 0; i < dim1->value(); ++i) {
+        for(unsigned int j = 0; j < dim2->value(); ++j) {
+            QString s1 = s1.number((i));
+            QString s2 = s2.number((j));
+            if(depart->item(i,j)->backgroundColor() == "black") {
+                saveUiContext("item_key_gol"+s1+","+s2,1);
+            }else{
+                saveUiContext("item_key_gol"+s1+","+s2,0);
+            }
+        }
+    }
+
+    int  speed_val = speed->value();
+    saveUiContext("speed_key_gol",speed_val);
+}
+
+
+void AutoCellGol::loadSettings(){
+    QVariant nb_n_val = loadUiContext("nb_n_key_gol",0);
+    nb_n->setValue(nb_n_val.toInt());
+
+    QVariant min_n_val = loadUiContext("min_n_key_gol",0);
+    min_n->setValue(min_n_val.toInt());
+
+    QVariant max_n_val = loadUiContext("max_n_key_gol",0);
+    max_n->setValue(max_n_val.toInt());
+
+    QVariant dim1_val = loadUiContext("dim1_key_gol",0);
+    dim1->setValue(dim1_val.toInt());
+
+    QVariant dim2_val = loadUiContext("dim2_key_gol",0);
+    dim2->setValue(dim2_val.toInt());
+
+    QVariant nb_step_val = loadUiContext("nb_step_key_gol",0);
+    nb_step->setValue(nb_step_val.toInt());
+
+    QVariant choice_grid_gol_val = loadUiContext("choice_grid_gol_key",0);
+    choiceGrid->setCurrentIndex(choice_grid_gol_val.toInt());
+
+    this->newGrid();
+
+    for(unsigned int i = 0; i < dim1->value(); ++i) {
+        for(unsigned int j = 0; j < dim2->value(); ++j) {
+            QString s1 = s1.number((i));
+            QString s2 = s2.number((j));
+
+            QVariant item_val_gol = loadUiContext("item_key_gol"+s1+","+s2,0);
+            if(item_val_gol.toInt()==0){
+                depart->item(i,j)->setText("");
+                depart->item(i,j)->setBackgroundColor("white");
+            }else{
+                depart->item(i,j)->setText("_");
+                depart->item(i,j)->setTextColor("black");
+                depart->item(i,j)->setBackgroundColor("black");
+            }
+        }
+    }
+
+    QVariant speed_val = loadUiContext("speed_key_gol",0);
+    speed->setValue(speed_val.toInt());
+}
+
+
+void AutoCellGol::saveAutom(){
+    GameOfLife gol(nb_n->value(),min_n->value(),max_n->value());
+    gol.accept(dbTest);
+    loadAutomList();
+}
+
+
+void AutoCellGol::loadAutomList(){
+    rankRules=0;
+    loadAutomaton->clear();
+    QSqlQuery query = dbTest.loadLifeAutomaton();
+    while (query.next())
+    {
+        std::vector<int> vec;
+        vec.push_back(query.value(0).toInt());
+        vec.push_back(query.value(1).toInt());
+        vec.push_back(query.value(2).toInt());
+        rules.push_back(vec);
+        loadAutomaton->addItem(query.value(0).toString()+" , "+query.value(1).toString()+" , "+query.value(2).toString(),rankRules);
+        rankRules++;
+    }
+}
+
+void AutoCellGol::choiceAutomChanged(){
+    nb_n->setValue(rules[loadAutomaton->currentData().toInt()][0]);
+    min_n->setValue(rules[loadAutomaton->currentData().toInt()][1]);
+    max_n->setValue(rules[loadAutomaton->currentData().toInt()][2]);
+}
+
+
